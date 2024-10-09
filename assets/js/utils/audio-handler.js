bf2f27d05d4f112d38a2c50a5737d0d033596e6d
zz plant <kanavj@gmail.com>
2024-10-09 17:10:51 -0500

    Update audio-handler.js

--- assets/js/utils/audio-handler.js
+++ assets/js/utils/audio-handler.js
@@ -1,17 +1,18 @@
 export async function initAudio() {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 256;
-    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+  const audioContext = new AudioContext();
+  const analyser = audioContext.createAnalyser();
+  analyser.fftSize = 256;
+  const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const audioSource = audioContext.createMediaStreamSource(stream);
-    audioSource.connect(analyser);
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const audioSource = audioContext.createMediaStreamSource(stream);
+  audioSource.connect(analyser);
 
-    return { analyser, dataArray };
+  return { analyser, dataArray };
 }
 
-export function getFrequencyData(analyser, dataArray) {
-    analyser.getByteFrequencyData(dataArray);
-    return dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;  // Average frequency
+export function getFrequencyData(analyser) {
+  const dataArray = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(dataArray);
+  return dataArray;
 }
