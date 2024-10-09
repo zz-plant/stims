ca5fd8a9be646232be4a7dbdce6c29d6ea8d8827
zz plant <kanavj@gmail.com>
2024-10-09 16:36:35 -0500

    Update renderer-setup.js

--- assets/js/core/renderer-setup.js
+++ assets/js/core/renderer-setup.js
@@ -1,11 +1,7 @@
 import * as THREE from 'three';
 
-export function initRenderer(canvas, { antialias = true, shadowMapEnabled = false, size = { width: window.innerWidth, height: window.innerHeight } } = {}) {
+export function initRenderer(canvas, { antialias = true, size = { width: window.innerWidth, height: window.innerHeight } } = {}) {
     const renderer = new THREE.WebGLRenderer({ canvas, antialias });
     renderer.setSize(size.width, size.height);
-    
-    // Enable or disable shadow mapping based on the configuration
-    renderer.shadowMap.enabled = shadowMapEnabled;
-
     return renderer;
 }
