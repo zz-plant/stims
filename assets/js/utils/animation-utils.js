2a8c65d776d7ace406e00030e6c71004ad7b4fec
zz plant <kanavj@gmail.com>
2024-10-09 15:54:59 -0500

    Delete assets/js/utils/animation-utils.js

--- assets/js/utils/animation-utils.js
+++
@@ -1,5 +0,0 @@
-export function applyAudioRotation(mesh, audioData) {
-    const rotationSpeed = audioData[0] / 255.0 * 0.05; // Maps frequency to a speed factor.
-    mesh.rotation.x += rotationSpeed;
-    mesh.rotation.y += rotationSpeed;
-}
