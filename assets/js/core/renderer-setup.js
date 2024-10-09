9e8ae5d1350276cdb3bdf2da571c1c6be7e67c77
zz plant <kanavj@gmail.com>
2024-10-09 17:09:35 -0500

    Update renderer-setup.js

--- assets/js/core/renderer-setup.js
+++ assets/js/core/renderer-setup.js
@@ -1,6 +1,6 @@
 // renderer-setup.js
 export function initRenderer(canvas, config = { antialias: true }) {
-    const renderer = new THREE.WebGLRenderer({ canvas, antialias: config.antialias });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    return renderer;
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: config.antialias });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  return renderer;
 }
