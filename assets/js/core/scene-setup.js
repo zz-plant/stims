3f684f0fd018492717b546d71c78ed9362184439
zz plant <kanavj@gmail.com>
2024-10-09 16:36:08 -0500

    Update scene-setup.js

--- assets/js/core/scene-setup.js
+++ assets/js/core/scene-setup.js
@@ -1,20 +1,8 @@
 import * as THREE from 'three';
 
-export function initScene() {
+// Initialize the Three.js scene
+export function initScene({ backgroundColor = 0x000000 }) {
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x222222);
+    scene.background = new THREE.Color(backgroundColor);
     return scene;
-}
-
-export function initPerspectiveCamera(fov = 75, aspect = window.innerWidth / window.innerHeight, near = 0.1, far = 1000) {
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 60);
-    return camera;
-}
-
-export function initRenderer(container) {
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-    return renderer;
 }
