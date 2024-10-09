bce3b07968f1ff865544e1838d2eb993723f5b55
zz plant <kanavj@gmail.com>
2024-10-09 14:25:03 -0500

    Update animation-utils.js

--- assets/js/animation-utils.js
+++ assets/js/animation-utils.js
@@ -3,9 +3,9 @@ export function applyAudioScale(object, audioData, sensitivity = 50) {
     const { lowFreq, midFreq, highFreq } = audioData;
 
     object.scale.set(
-        1 + lowFreq / 256 * sensitivity / 100,
-        1 + midFreq / 256 * sensitivity / 100,
-        1 + highFreq / 256 * sensitivity / 100
+        1 + (lowFreq / 256) * (sensitivity / 100),
+        1 + (midFreq / 256) * (sensitivity / 100),
+        1 + (highFreq / 256) * (sensitivity / 100)
     );
 }
 
@@ -13,18 +13,18 @@ export function applyAudioScale(object, audioData, sensitivity = 50) {
 export function applyAudioRotation(object, audioData, sensitivity = 1) {
     const { lowFreq, midFreq, highFreq } = audioData;
 
-    object.rotation.x += lowFreq / 256 * sensitivity;
-    object.rotation.y += midFreq / 256 * sensitivity;
-    object.rotation.z += highFreq / 256 * sensitivity;
+    object.rotation.x += (lowFreq / 256) * sensitivity;
+    object.rotation.y += (midFreq / 256) * sensitivity;
+    object.rotation.z += (highFreq / 256) * sensitivity;
 }
 
 // Apply color changes based on audio data
 export function applyAudioColorChange(object, audioData, sensitivity = 1) {
     const { lowFreq, midFreq, highFreq } = audioData;
 
-    const r = lowFreq / 256 * sensitivity;
-    const g = midFreq / 256 * sensitivity;
-    const b = highFreq / 256 * sensitivity;
+    const r = (lowFreq / 256) * sensitivity;
+    const g = (midFreq / 256) * sensitivity;
+    const b = (highFreq / 256) * sensitivity;
 
     object.material.color.setRGB(r, g, b);
 }
