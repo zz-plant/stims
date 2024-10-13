db94ce830acd97a0b73947afbc1b9e413b4f860c
zz plant <kanavj@gmail.com>
2024-10-13 00:39:11 -0500

    Update audio-handler.js

--- assets/js/utils/audio-handler.js
+++ assets/js/utils/audio-handler.js
@@ -1,18 +1,23 @@
 export async function initAudio() {
-  const audioContext = new AudioContext();
-  const analyser = audioContext.createAnalyser();
-  analyser.fftSize = 256;
-  const dataArray = new Uint8Array(analyser.frequencyBinCount);
+    try {
+        const audioContext = new AudioContext();
+        const analyser = audioContext.createAnalyser();
+        analyser.fftSize = 256;
+        const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  const audioSource = audioContext.createMediaStreamSource(stream);
-  audioSource.connect(analyser);
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const audioSource = audioContext.createMediaStreamSource(stream);
+        audioSource.connect(analyser);
 
-  return { analyser, dataArray };
+        return { analyser, dataArray };
+    } catch (error) {
+        console.error('Error accessing audio:', error);
+        throw new Error('Microphone access was denied.');
+    }
 }
 
 export function getFrequencyData(analyser) {
-  const dataArray = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteFrequencyData(dataArray);
-  return dataArray;
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
+    return dataArray;
 }
