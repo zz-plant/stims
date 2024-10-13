e25210d00efd9644dac8ff74caab37e3d5a98d61
zz plant <kanavj@gmail.com>
2024-10-13 00:42:00 -0500

    Update lighting-setup.js

--- assets/js/lighting/lighting-setup.js
+++ assets/js/lighting/lighting-setup.js
@@ -1,11 +1,47 @@
 // lighting-setup.js
-export function initLighting(scene, config = { color: 0xffffff, intensity: 1, position: { x: 10, y: 10, z: 10 } }) {
-  const light = new THREE.PointLight(config.color, config.intensity);
-  light.position.set(config.position.x, config.position.y, config.position.z);
-  scene.add(light);
+
+// Initialize a point light with configurable options
+export function initLighting(scene, config = { 
+    type: 'PointLight', // Allow different types of lights
+    color: 0xffffff, 
+    intensity: 1, 
+    position: { x: 10, y: 10, z: 10 }, 
+    castShadow: false // Enable shadow support
+}) {
+    let light;
+
+    switch (config.type) {
+        case 'DirectionalLight':
+            light = new THREE.DirectionalLight(config.color, config.intensity);
+            light.position.set(config.position.x, config.position.y, config.position.z);
+            if (config.castShadow) {
+                light.castShadow = true;
+            }
+            break;
+        case 'SpotLight':
+            light = new THREE.SpotLight(config.color, config.intensity);
+            light.position.set(config.position.x, config.position.y, config.position.z);
+            if (config.castShadow) {
+                light.castShadow = true;
+            }
+            break;
+        case 'HemisphereLight':
+            light = new THREE.HemisphereLight(config.color, 0x444444, config.intensity);
+            break;
+        default:
+            light = new THREE.PointLight(config.color, config.intensity);
+            light.position.set(config.position.x, config.position.y, config.position.z);
+            if (config.castShadow) {
+                light.castShadow = true;
+            }
+            break;
+    }
+
+    scene.add(light);
 }
 
 export function initAmbientLight(scene, config = { color: 0x404040, intensity: 0.5 }) {
-  const ambientLight = new THREE.AmbientLight(config.color, config.intensity);
-  scene.add(ambientLight);
+    const ambientLight = new THREE.AmbientLight(config.color, config.intensity);
+    scene.add(ambientLight);
 }
+
