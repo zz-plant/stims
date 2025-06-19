806b6f72379d6abc2523319e4020eac21bfb1a54
zz plant <kanavj@gmail.com>
2025-06-19 14:36:37 -0500

    Add React setup and components

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
   esbuild: {
     loader: 'ts',
-    include: /assets\/js\/.*\.ts$/,
+    include: /assets\/js\/.*\.[tj]sx?$/,
   },
 });
