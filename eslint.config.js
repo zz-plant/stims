aa1a12d2d7c61f63e7f78ef733f118ba37a577a1
zz plant <kanavj@gmail.com>
2025-12-17 17:03:52 -0600

    Add TypeScript ESLint support

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
-  js.configs.recommended,
   {
     files: ['**/*.{js,ts,tsx}'],
     languageOptions: {
@@ -25,6 +26,22 @@ export default [
         CustomEvent: 'readonly',
       },
     },
-    ignores: ['assets/js/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
   },
 ];
